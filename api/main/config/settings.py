f46cb68e1e4bac3ef1f57271e00cff51fc69a376
hafiz-bs23 <hafizur.bs23@gmail.com>
2024-08-26 03:32:01 +0600

    Add ci cd workflow

--- api/main/config/settings.py
+++ api/main/config/settings.py
@@ -1,5 +1,6 @@
-from pydantic_settings import BaseSettings
-class Settings(BaseSettings):
+import pydantic_settings
+class Settings(pydantic_settings.BaseSettings):
     origins: list = ["http://localhost", "http://localhost:8080"]
+    
     
 settings = Settings()
